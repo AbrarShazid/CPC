27ef540ce0bbd0d148ee927a668973d8414bfd3f
Abrar Agmain Shazid <137707705+AbrarShazid@users.noreply.github.com>
2023-07-26 23:04:00 +0600

    Create Task-3.cpp

---
+++ Class-12/Task-3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef double dl;
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lk() cout.unsetf(ios::floatfield);cout.precision(3);cout.setf(ios::fixed,ios::floatfield);
+int person[61];
+int main() {
+    optimize();
+    lk();
+
+
+cout<<"Welcome to ACM ICPC Dhaka Regional Online Preliminary Contest, 2018"<<endl;
+
+return 0;
+
+}

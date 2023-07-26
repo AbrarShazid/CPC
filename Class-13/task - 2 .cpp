eb2097f7d67d49948f95e73b551745d79ee23b83
Abrar Agmain Shazid <137707705+AbrarShazid@users.noreply.github.com>
2023-07-27 01:15:59 +0600

    Create task - 2.cpp

---
+++ Class-13/task - 2 .cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef double dl;
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lk() cout.unsetf(ios::floatfield);cout.precision(3);cout.setf(ios::fixed,ios::floatfield);
+int person[61];
+int main() {
+    optimize();
+    lk();
+
+int a,b;
+cin>>a>>b;
+if(a>=b){
+    cout<<"Yes"<<endl;
+}
+else{
+    cout<<"No"<<endl;
+}
+
+
+
+return 0;
+
+}

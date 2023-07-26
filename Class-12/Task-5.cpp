4334418cccf84ee577e2ab9b9516c9b53d6d36f9
Abrar Agmain Shazid <137707705+AbrarShazid@users.noreply.github.com>
2023-07-27 01:08:49 +0600

    Create Task-5.cpp

---
+++ Class-12/Task-5.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef double dl;
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lk() cout.unsetf(ios::floatfield);cout.precision(3);cout.setf(ios::fixed,ios::floatfield);
+int person[61];
+int main() {
+    optimize();
+    lk();
+
+cout<<68<<" "<<0<<" "<<0<<endl;
+return 0;
+
+}
